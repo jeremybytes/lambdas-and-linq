30a771b28235e397106bd5f6933761d6f38aa4ed
Jeremy Clark <jeremy@jeremybytes.com>
2015-02-26 18:24:26 -0800

    Added custom class for OrderReports output

--- JoinSyntax/OrderReports.cs
+++ JoinSyntax/OrderReports.cs
@@ -4,25 +4,37 @@ using System.Linq;
 
 namespace JoinSyntax
 {
+    public class CustomerOrderDates
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+
     public static class OrderReports
     {
-        public static IEnumerable<dynamic> 
+        public static IEnumerable<CustomerOrderDates>
             OrderDatesByCustomer1(DateTime startDate, DateTime endDate)
         {
             var people = People.GetPeople();
             var orders = Orders.GetOrders();
 
-            var orderDates =  from p in people
-                              join o in orders on p.Id equals o.CustomerId
-                              where o.OrderDate >= startDate &&
-                                    o.OrderDate <= endDate
-                              orderby o.OrderDate
-                              select new { p.LastName, p.FirstName, o.OrderDate };
-            
+            var orderDates = from p in people
+                             join o in orders on p.Id equals o.CustomerId
+                             where o.OrderDate >= startDate &&
+                                   o.OrderDate <= endDate
+                             orderby o.OrderDate
+                             select new CustomerOrderDates
+                               {
+                                   LastName = p.LastName,
+                                   FirstName = p.FirstName,
+                                   OrderDate = o.OrderDate
+                               };
+
             return orderDates;
         }
 
-        public static IEnumerable<dynamic> 
+        public static IEnumerable<CustomerOrderDates>
             OrderDatesByCustomer2(DateTime startDate, DateTime endDate)
         {
             var people = People.GetPeople();
@@ -32,7 +44,12 @@ namespace JoinSyntax
                     orders,
                     p => p.Id,
                     o => o.CustomerId,
-                    (p, o) => new { p.LastName, p.FirstName, o.OrderDate })
+                    (p, o) => new CustomerOrderDates
+                                {
+                                    LastName = p.LastName,
+                                    FirstName = p.FirstName,
+                                    OrderDate = o.OrderDate
+                                })
                 .Where(r => r.OrderDate >= startDate && r.OrderDate <= endDate)
                 .OrderBy(r => r.OrderDate);
 
