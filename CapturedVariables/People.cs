210dc5e07fc87180e2cdbc1f558a1b4c0506c128
Jeremy Clark <jeremy@jeremybytes.com>
2015-03-15 13:04:07 -0700

    New project to show quirks in using captured variables

---
+++ CapturedVariables/People.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapturedVariables
+{
+    public static class People
+    {
+        public static List<Person> GetPeople()
+        {
+            var people = new List<Person>
+            {
+                new Person() { Id=1, FirstName="John", LastName="Koenig",
+                    StartDate = new DateTime(1975, 10, 17), Rating=6 },
+                new Person() { Id=2, FirstName="Dylan", LastName="Hunt", 
+                    StartDate = new DateTime(2000, 10, 2), Rating=8 },
+                new Person() { Id=3, FirstName="John", LastName="Crichton", 
+                    StartDate = new DateTime(1999, 3, 19), Rating=7 },
+                new Person() { Id=4, FirstName="Dave", LastName="Lister", 
+                    StartDate = new DateTime(1988, 2, 15), Rating=9 },
+                new Person() { Id=5, FirstName="John", LastName="Sheridan", 
+                    StartDate = new DateTime(1994, 1, 26), Rating=6 },
+                new Person() { Id=6, FirstName="Dante", LastName="Montana", 
+                    StartDate = new DateTime(2000, 11, 1), Rating=5 },
+                new Person() { Id=7, FirstName="Isaac", LastName="Gampu", 
+                    StartDate = new DateTime(1977, 9, 10), Rating=4 }
+            };
+            return people;
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Rating { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1} {2}", Id, FirstName, LastName);
+        }
+    }
+}
---
+++ CapturedVariables/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapturedVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = People.GetPeople();
+            BadCapture(people);
+            GoodCapture(people);
+            Console.ReadLine();
+        }
+
+        // Captured variables have the value at the time of *use*
+        // not the value at the time of capture.
+        private static void BadCapture(List<Person> people)
+        {
+            Task[] tasks = new Task[people.Count()];
+            for(int i = 0; i < people.Count(); i++)
+            {
+                // The indexer (i) is captured. This will have
+                // a value of "7", the value of the indexer 
+                // at the end of the "for" loop.
+                tasks[i] = Task.Run(() => OutputPerson(people, i));
+            }
+            Task.WaitAll(tasks);
+        }
+
+        private static void GoodCapture(List<Person> people)
+        {
+            Task[] tasks = new Task[people.Count()];
+            for (int i = 0; i < people.Count(); i++)
+            {
+                // A local variable that has a copy of the 
+                // indexer (i) is captured. This creates a 
+                // different variable for each iteration of 
+                // the "for" loop, and the expected value is 
+                // captured.
+                int capturedIndex = i;
+                tasks[i] = Task.Run(() => OutputPerson(people, capturedIndex));
+            }
+            Task.WaitAll(tasks);
+        }
+
+        private static void OutputPerson(List<Person> people, int index)
+        {
+            try
+            {
+                Console.WriteLine(people[index].ToString());
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Argument out of range. Actual value: {0}", index);
+            }
+        }
+    }
+}
